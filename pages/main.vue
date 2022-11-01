228135dad5a0fb3d7296e42c7b4b919ab96a1b0c
jungwon <rhehf1024@naver.com>
2022-11-02 00:59:01 +0900

    ~ axios 설치 및 API 호출

---
+++ pages/main.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>메인페이</h1>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+export  default {
+  async created() {
+    const response = await axios.get('https://localhost3000/products')
+    console.log(response)
+  }
+}
+
+
+</script>
